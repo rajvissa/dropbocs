105e8bfafcaac5af454fdffa5640f23af61b382d
rajendra <rajendra@dd8eb467-b960-5271-f399-3b5d354d76b8>
2011-10-08 04:55:17 +0000

    Initial code

---
+++ trunk/src/com/oneverse/dropbocs/DropBocsActivity.java
@@ -0,0 +1,15 @@
+package com.oneverse.dropbocs;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class DropBocsActivity extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
