d06d5417fe089e3593d9d0cc4c98578fc1860a9a
rajendra <rajendra@dd8eb467-b960-5271-f399-3b5d354d76b8>
2011-10-08 05:29:03 +0000

    Initial code

--- trunk/src/com/oneverse/dropbocs/DropBocsActivity.java
+++ trunk/src/com/oneverse/dropbocs/DropBocsActivity.java
@@ -1,15 +1,34 @@
 package com.oneverse.dropbocs;
 
+import android.app.ActionBar;
 import android.app.Activity;
+import android.app.FragmentTransaction;
 import android.os.Bundle;
 
-public class DropBocsActivity extends Activity
-{
-    /** Called when the activity is first created. */
+public class DropBocsActivity extends Activity implements ActionBar.TabListener {
+    /**
+     * Called when the activity is first created.
+     */
     @Override
-    public void onCreate(Bundle savedInstanceState)
-    {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+        setContentView(com.oneverse.dropbocs.R.layout.main);
+        ActionBar bar = getActionBar();
+
+        bar.addTab(bar.newTab().setText("DropBox").setTabListener(this));
+        bar.addTab(bar.newTab().setText("Google Docs").setTabListener(this));
+        bar.addTab(bar.newTab().setText("Box.net").setTabListener(this));
+    }
+
+    public void onTabSelected(ActionBar.Tab tab, FragmentTransaction fragmentTransaction) {
+        //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    public void onTabUnselected(ActionBar.Tab tab, FragmentTransaction fragmentTransaction) {
+        //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    public void onTabReselected(ActionBar.Tab tab, FragmentTransaction fragmentTransaction) {
+        //To change body of implemented methods use File | Settings | File Templates.
     }
 }
